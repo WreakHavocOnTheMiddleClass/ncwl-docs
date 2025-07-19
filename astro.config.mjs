5d1fe0f71de806038e1af55c8716c52a58e9a852
WreakHavocOnTheMiddleClass <179751402+WreakHavocOnTheMiddleClass@users.noreply.github.com>
2025-07-19 10:26:59 -0500

    configure redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://wreakhavoconthemiddleclass.github.io',
+    base: 'ncwl-docs'
+});
